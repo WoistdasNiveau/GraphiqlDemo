ae3eb3a1a974c1370f6270c2b4880e7efbc0e910
oliverstockl <oliver01@kabsi.at>
2023-05-02 19:41:38 +0200

    Initial commit

---
+++ src/main/java/com/oegeg/etd/saveservicetry2/SaveServiceTry2Application.java
@@ -0,0 +1,13 @@
+package com.oegeg.etd.saveservicetry2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveServiceTry2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveServiceTry2Application.class, args);
+    }
+
+}
